e9f4a209d059b1d574922a1b7287643baf3047e3
Javier1600 <javiandrade83@gmail.com>
2022-07-19 17:43:42 -0500

    Proyecto

--- proyecto/src/Components/Login.jsx
+++ proyecto/src/Components/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import avatar from '../img/usuario.png'
 
 import '../Styles/loginStyle.css';
 function Login(){
     return(
     <div className="login-box">
-      <img src='https://cdn-icons.flaticon.com/png/512/3795/premium/3795330.png?token=exp=1658181314~hmac=9dea66d25e9613579388ddadff479baf' alt="Avatar" className="avatar" ></img>
+      <img src={avatar} alt="Avatar" className="avatar" ></img>
       <label for="username">Username</label>
         <input type="text" placeholder="Enter Username"></input>
         <label for="password">Password</label>
